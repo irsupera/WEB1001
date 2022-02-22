6efb34000ef59b28ff07241b0af99e1edb80811c
irsupera <97858090+irsupera@users.noreply.github.com>
2022-02-22 02:19:59 -0500

    Web App
    Created my first web app

---
+++ WebApp1/Pages/Intro.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp1.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public int Number1 { get; set; } = 0;
+        public int Number2 { get; set; } = 0;
+
+        public void OnGet()
+        {
+        }
+    }
+}
